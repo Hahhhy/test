1fd6f30ff272ebd575275059cd828e0cb983a415
guozhaozhi <3558509716@qq.com>
2026-03-08 17:14:25 +0800

    公共字段自动填充业务开发

--- sky-server/src/main/java/com/sky/annotation/aspect/AutoFillAspect.java
+++ sky-server/src/main/java/com/sky/annotation/aspect/AutoFillAspect.java
@@ -27,7 +27,7 @@ public class AutoFillAspect {
      * 切入点
      */
     @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
-    public  void autoFillPointCut(){}
+    public void autoFillPointCut(){}
 
     /**
      * 前置通知，在方法执行前进行执行
